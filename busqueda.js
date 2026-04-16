9e9d11c579a5c69569a27a8a561b37f81963401e
Juan Gotz <juaaaangotz@gmail.com>
2026-04-16 20:13:41 -0300

    Cambios al mapa y a la busqueda
    QoL: Talleres son borrados cuando no estan en la busqueda, pase lo del mapa al .js, y esas weas.

--- busqueda.js
+++ busqueda.js
@@ -1,16 +1,49 @@
     document.addEventListener("DOMContentLoaded", main());
 function main() {
+    var map = L.map("map").setView([-34.52, -58.71], 13);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: "&copy; OpenStreetMap",
+        }).addTo(map);
     const searchBar = document.getElementById("buscador");
     const talleres = document.querySelectorAll(".taller-card");
+    const markers = [];
+    talleres.forEach(function(taller){
+        marcarTallerEnMapa(taller);
+    });
     searchBar.addEventListener("change", () => {
+        busquedaTalleres(searchBar.value.toLowerCase());
+    });
+
+    function busquedaTalleres(string){
         let busqueda = searchBar.value.toLowerCase();
-        talleres.forEach(function(taller){
+        markers.forEach(function (layer) {
+            map.removeLayer(layer);
+        });
+        talleres.forEach(function(taller) {
             let nombre = taller.innerHTML.toLowerCase();
-            if(nombre.includes(busqueda)){
+            if(nombre.includes(string)){
                 taller.style.display = "block";
+                marcarTallerEnMapa(taller);
             } else {
                 taller.style.display = "none";
+                quitarTallerEnMapa(taller);
             }
         });
-    });
+    };
+
+    function marcarTallerEnMapa(taller){
+            let latitud = parseFloat(taller.dataset.lat);
+            let longitud = parseFloat(taller.dataset.lon);
+            let id = parseFloat(taller.dataset.id);
+            markers[id] = L.marker([latitud, longitud]).addTo(map);
+            console.log("id añadido: " + id);
+        };
+
+          
+    function quitarTallerEnMapa(taller){
+        let id = parseFloat(taller.dataset.id);
+        map.removeLayer(markers[id]);
+        console.log("id borrado: " + id);
+    }
 }
